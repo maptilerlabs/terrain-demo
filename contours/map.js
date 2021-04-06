96a221145c1f237aef792d54ee52bc66213f74fe
Jachym Cepicky <jachym.cepicky@gmail.com>
2021-04-06 13:31:09 +0200

    adding mt key

--- contours/map.js
+++ contours/map.js
@@ -1,6 +1,5 @@
 import { Map, GeolocateControl } from 'https://cdn.skypack.dev/maplibre-gl'
 
-//const mapTilerKey = "olPbAXB9QkZuFSDG4x2V"
 const mapTilerKey = "XFw43ruuvATFrAeR6eeg"
 
 const backgroundMap = new Map({
@@ -13,7 +12,7 @@ const backgroundMap = new Map({
 
 const beforeMap = new Map({
   container: 'before',
-  style: `https://api.maptiler.com/maps/2c83d77f-6309-404c-a00b-ab6a1c47b21b/style.json?key=XFw43ruuvATFrAeR6eeg`,
+  style: `https://api.maptiler.com/maps/2c83d77f-6309-404c-a00b-ab6a1c47b21b/style.json?key=${mapTilerKey}`,
   center: [0, 0],
   hash: true,
   zoom: 0
@@ -21,7 +20,7 @@ const beforeMap = new Map({
 
 const afterMap = new Map({
   container: 'after',
-  style: `https://api.maptiler.com/maps/48315e60-7931-47e9-8118-32f937f1823f/style.json?key=XFw43ruuvATFrAeR6eeg`,
+  style: `https://api.maptiler.com/maps/48315e60-7931-47e9-8118-32f937f1823f/style.json?key=${mapTilerKey}`,
   center: [0, 0],
   hash: true,
   zoom: 0
